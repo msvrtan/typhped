9bad1a369ad3b08deab3e42b601faf590f8ab21e
Miro Svrtan <miro@mirosvrtan.me>
2018-11-11 18:17:54 +0100

    Setup nikic php parser to see what is happening

---
+++ tests/MirkoTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Generator;
+use PhpParser\Error;
+use PhpParser\NodeDumper;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter;
+use PHPUnit\Framework\TestCase;
+
+class MirkoTest extends TestCase
+{
+    /**
+     * @dataProvider provideCode
+     */
+    public function testNikicParserOutput(string $code): void
+    {
+        $parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+        try {
+            /** @var \PhpParser\Node[] $ast */
+            $ast = $parser->parse($code);
+        } catch (Error $error) {
+            echo "Parse error: {$error->getMessage()}\n";
+
+            return;
+        }
+
+        $dumper = new NodeDumper();
+        echo $dumper->dump($ast)."\n";
+
+        $prettyPrinter = new PrettyPrinter\Standard();
+        echo $prettyPrinter->prettyPrintFile($ast);
+    }
+
+    public function provideCode(): Generator
+    {
+        yield[file_get_contents(__DIR__.'/../src/Example/SimpleClass.php')];
+        //yield[file_get_contents(__DIR__.'/../src/Example/JsonSerializableClass.php')];
+        //yield[file_get_contents(__DIR__.'/../src/Example/JsonSerializableClassWithImports.php')];
+    }
+}
